757841df13f3eb88458bc031f067ba0b89a20e5b
DOMAIN\tkot <tommi.korhonen@thl.fi>
2023-05-30 15:58:21 +0300

    new version

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
+$param_name = isset($_REQUEST["name"]) ? trim($_REQUEST["name"]) : "";
+?>
 
-echo "Hello World!";
+<html>
+
+<head>
+    <title>Hello App</title>
+
+<body>
+    <h1>Hello</h1>
+    What is your name?
+    <form method="POST" action="<?php print $_SERVER["PHP_SELF"] ?>">
+        <input type="text" name="name" value="<?php print $param_name ?>">
+        <input type="submit" value="Say Hello">
+    </form>
+
+    <?php
+    if ($param_name != "") {
+        print "<h2>Hello " . $param_name . " !</h2>";
+    }
+    ?>
+</body>
+
+</html>
